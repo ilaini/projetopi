eeff7cb2ea745108fc239f10f717ceb29664a94d
20161204010027 <20161204010027@LAJ291357.ifrn.local>
2019-11-29 16:50:37 -0200

    Clonando o projeto.

---
+++ VestUrbanWear/src/main/java/com/VestUrbanWear/VestUrbanWearApplication.java
@@ -0,0 +1,13 @@
+package com.VestUrbanWear;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VestUrbanWearApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VestUrbanWearApplication.class, args);
+	}
+
+}
---
+++ VestUrbanWear/src/test/java/com/VestUrbanWear/VestUrbanWear/VestUrbanWearApplicationTests.java
@@ -0,0 +1,13 @@
+package com.VestUrbanWear.VestUrbanWear;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VestUrbanWearApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
